baa782f6fbca92ec98cf2fe8121649fe991a3921
EmmaC <c.emma2112@gmail.com>
2022-08-08 13:43:36 +0000

    add variables that hold letters & max key kenght. Add general notes to keep track of functions/vars that need to be created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded",function() 
 {
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let max_key_length = letters.length
+    console.log(letters);
+    console.log(max_key_length);
+
     let buttons = document.getElementsByTagName("button");
 
     for(let button of buttons)
@@ -9,4 +14,16 @@ document.addEventListener("DOMContentLoaded",function()
             console.log("the button has been clicked");
         })
     }
-})
+
+}
+)
+
+//variable that stores all letters user can input
+//variable that covers the max key length
+// get encrypt input
+// get encrypt key
+// get decrypt input
+// get decrypt key
+// encryptMessage
+// decryptMessage
+
