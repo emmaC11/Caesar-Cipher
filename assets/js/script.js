b23b833a2618a17a6ccb0f0cbcff081cf5a9d935
EmmaC <c.emma2112@gmail.com>
2022-08-23 12:18:03 +0000

    add semicolon & format JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-let max_key_length = letters.length
+let max_key_length = letters.length;
 let shiftedLetters = "";
 
 
@@ -16,26 +16,26 @@ let message = document.getElementById('message');
 let key = document.getElementById('key').value;
 let ciphertext = document.getElementById('ciphertext');
 const heading = document.getElementById('main-title');
-const introText = document.getElementById('user-options')
+const introText = document.getElementById('user-options');
 const messageLabel = document.getElementById('messageLabel');
 const outputLabel = document.getElementById('outputLabel');
 const encryptDecryptButton = document.getElementById('encrypt-decrypt');
 const returnArrow = document.getElementById('return');
-let cipherMode = ""
+let cipherMode = "";
 
 //footer
 const footer = document.querySelector('footer');
 
 //HELP PAGE TYPEWRITER EFFECTS
 const typeText = document.getElementById('typetext');
-const helpPhrases = ['Click button below', 'Click! Click!', 'Click Now!']; 
+const helpPhrases = ['Click button below', 'Click! Click!', 'Click Now!'];
 
 //item in array
-let i = 0
+let i = 0;
 //letter
-let x = 0
+let x = 0;
 let currentPhrase = [];
-let isDeletingLetter = false
+let isDeletingLetter = false;
 
 //LTYPEWRITER FUNCTION
 function typewriter() {
@@ -43,49 +43,47 @@ function typewriter() {
    typeText.innerText = currentPhrase.join('');
 
    //loop through items in phrases array
-   if(i < helpPhrases.length){
+   if (i < helpPhrases.length) {
 
       //push letters to currentPhrase array
-      if(!isDeletingLetter && x <= helpPhrases[i].length){
-         currentPhrase.push(helpPhrases[i][x])
+      if (!isDeletingLetter && x <= helpPhrases[i].length) {
+         currentPhrase.push(helpPhrases[i][x]);
          x++;
       }
 
       //if reach end of phrase, need to delete letters 
-      if(isDeletingLetter && x <= helpPhrases[i].length){
-         currentPhrase.pop(helpPhrases[i][x])
+      if (isDeletingLetter && x <= helpPhrases[i].length) {
+         currentPhrase.pop(helpPhrases[i][x]);
          x--;
       }
 
       // if x is equal to the length of the first phrase, increment to move to next item in array
-      if(x == helpPhrases[i].length){
+      if (x == helpPhrases[i].length) {
          //need to delete existing phrase before moving onto next
          isDeletingLetter = true;
       }
 
       //when all letters are removed, move onto next item in array, now adding letters
-      if(isDeletingLetter && x === 0){
-         currentPhrase = []
+      if (isDeletingLetter && x === 0) {
+         currentPhrase = [];
          isDeletingLetter = false
-         i++
+         i++;
 
          //if reach end of array, reset back to 0
-         if(i === helpPhrases.length){
-            i = 0
+         if (i === helpPhrases.length) {
+            i = 0;
          }
       }
 
-      
+
    }
 
-   if(isDeletingLetter)
-   {
-      setTimeout(typewriter,150)
+   if (isDeletingLetter) {
+      setTimeout(typewriter, 150);
+   } else {
+      setTimeout(typewriter, 200);
    }
-   else{
-      setTimeout(typewriter,200)
-   }
-    
+
 }
 typewriter();
 
@@ -94,27 +92,27 @@ typewriter();
 function modeEncrypt() {
    let mode = "encrypt";
    console.log(mode);
-   return mode
+   return mode;
 
 }
 
 function modeDecrypt() {
    let mode = "decrypt";
    console.log(mode);
-   return mode
+   return mode;
 }
 
 
 //HIDE BUTTONS WHEN CLICKED
 function hideButtons() {
-   if (encrypt.style.display === "none" && decrypt.style.display === "none"  && introText.style.display === "none") {
+   if (encrypt.style.display === "none" && decrypt.style.display === "none" && introText.style.display === "none") {
       encrypt.style.display = "block";
       decrypt.style.display = "block";
-      introText.style.display = "block"
+      introText.style.display = "block";
    } else {
-      encrypt.style.display = "none"
-      decrypt.style.display = "none"
-      introText.style.display = "none"
+      encrypt.style.display = "none";
+      decrypt.style.display = "none";
+      introText.style.display = "none";
    }
 }
 
@@ -123,10 +121,10 @@ function hideButtons() {
 function displayCipher() {
    if (cipherContent.style.display === "none") {
       cipherContent.style.display = "flex";
-      console.log("this is the if")
+      console.log("this is the if");
    } else {
       cipherContent.style.display = "none";
-      console.log("this is the else")
+      console.log("this is the else");
    }
 
 }
@@ -136,7 +134,7 @@ function getMessage() {
    // get text from input fields
    let message = document.getElementById('message').value;
    console.log(message);
-   return message
+   return message;
 
 }
 
@@ -145,133 +143,128 @@ function getKey() {
 
    let key = +document.getElementById('key').value;
    if (key >= 1 && key <= max_key_length) {
-      console.log("key is returned")
-      return key
+      console.log("key is returned");
+      return key;
    } else {
       //alert(`The key value must be greater than 0 and less than ${max_key_length}`);
 
    }
    console.log(key);
-   console.log(typeof key)
+   console.log(typeof key);
 }
 
 
 //ENCRYPT/DECRYPT MESSAGE
 function encryptMessage(message, key) {
 
-   let translated = ""
+   let translated = "";
 
    //LOOP THROUGH EACH CHARACTER OF MESSAGE
    for (let letter of message) {
 
       //GETS INDEX OF LETTER IN LETTERS STRING
-      let letterIndex = letters.indexOf(letter)
-      console.log(letterIndex)
+      let letterIndex = letters.indexOf(letter);
+      console.log(letterIndex);
 
       //IF LETTER IS NOT RECOGNISED IE IS A SYMBOL OR NUMBER STAYS THE SAME
       if (letterIndex == -1) {
-         translated += letter
+         translated += letter;
       }
       //SHIFT LETTER BY KEY NUMBER
       else {
-         letterIndex += key
+         letterIndex += key;
          console.log(letterIndex);
       }
 
       //EXCEPTION HANDLING IF LETTER EXCEEDS LENGHT OF LETTER STRING DUE TO KEY VALUE
       if (letterIndex >= letters.length) {
          letterIndex -= letters.length;
-         console.log(letterIndex)
+         console.log(letterIndex);
       } else if (letterIndex < 0) {
          //letterIndex += letters.length
-         console.log(letterIndex)
+         console.log(letterIndex);
       }
 
       //ADDS THE SHIFTED LETTERS TO THE TRANSLATED VARIABLE
-      translated += letters[letterIndex]
+      translated += letters[letterIndex];
 
-  
+
    }
 
    console.log(translated);
 
-   if(translated.includes("undefined")){
-      let newTranslated = translated.replaceAll('undefined','')
-      console.log("this is the replace all if")
+   if (translated.includes("undefined")) {
+      let newTranslated = translated.replaceAll('undefined', '');
+      console.log("this is the replace all if");
       ciphertext.textContent = newTranslated;
-   }
-   else{
-      ("this is the replace all else")
+   } else {
+      ("this is the replace all else");
       ciphertext.textContent = translated;
    }
 
-   
-   
-       
-   console.log(message)
-   console.log(key)
+
+
+
+   console.log(message);
+   console.log(key);
 }
 
-function decryptMessage(message,key)
-{
+function decryptMessage(message, key) {
    let translated = ""
-   key = -key
+   key = -key;
 
    for (let letter of message) {
 
       //GETS INDEX OF LETTER IN LETTERS STRING
-      let letterIndex = letters.indexOf(letter)
-      console.log(letterIndex)
+      let letterIndex = letters.indexOf(letter);
+      console.log(letterIndex);
 
       //IF LETTER IS NOT RECOGNISED IE IS A SYMBOL OR NUMBER STAYS THE SAME
       if (letterIndex == -1) {
-         translated += letter
+         translated += letter;
       }
       //SHIFT LETTER BY KEY NUMBER
       else {
-         letterIndex += key
+         letterIndex += key;
          console.log(letterIndex);
       }
 
       //EXCEPTION HANDLING IF LETTER EXCEEDS LENGHT OF LETTER STRING DUE TO KEY VALUE
       if (letterIndex >= letters.length) {
          letterIndex -= letters.length;
-         console.log(letterIndex)
+         console.log(letterIndex);
       } else if (letterIndex < 0 && letterIndex != -1) {
          letterIndex += letters.length
-         console.log(letterIndex)
+         console.log(letterIndex);
       }
 
       //ADDS THE SHIFTED LETTERS TO THE TRANSLATED VARIABLE
-      translated += letters[letterIndex]
+      translated += letters[letterIndex];
    }
 
    console.log(translated);
-   if(translated.includes("undefined")){
-      let newTranslated = translated.replaceAll('undefined','')
-      console.log("this is the replace all if")
+   if (translated.includes("undefined")) {
+      let newTranslated = translated.replaceAll('undefined', '')
+      console.log("this is the replace all if");
       ciphertext.textContent = newTranslated;
-   }
-   else{
+   } else {
       ("this is the replace all else")
       ciphertext.textContent = translated;
    }
-   
+
 
 }
 
 //ENCRYPT BUTTON EVENT LISTENER
 encrypt.addEventListener('click', () => {
-   console.log("this is the encrypt event listerner")
+   console.log("this is the encrypt event listerner");
    modeEncrypt();
    displayCipher();
-   heading.innerText = "Encrypt"
-   if(heading.innerText === "Encrypt")
-   {
-      cipherMode = "encrypt"
-   }
-   else{
-      console.log("the heading is not encrypt")
+   heading.innerText = "Encrypt";
+   if (heading.innerText === "Encrypt") {
+      cipherMode = "encrypt";
+   } else {
+      console.log("the heading is not encrypt");
    }
 
    //DISPLAY RETURN ARROW WHEN CLICKED
@@ -287,11 +280,11 @@ decrypt.addEventListener('click', () => {
    heading.innerText = "Decrypt";
    messageLabel.innerText = "Message to decrypt:";
    encryptDecryptButton.innerText = "Decrypt"
-   outputLabel.innerText = "Decrypted Text:"
+   outputLabel.innerText = "Decrypted Text:";
 
    //DISPLAY RETURN ARROW WHEN CLICKED
    returnArrow.style.display = "block";
-   footer.style.backgroundImage = "none"
+   footer.style.backgroundImage = "none";
 
 })
 
@@ -299,14 +292,14 @@ decrypt.addEventListener('click', () => {
 //STORE CIPHERTEXT OUTPUT IN VARIABLE & DISPLAY CHANGES IN FUNCTION
 let ciphertextoutput = document.getElementById('output');
 let spinner = document.getElementById('load');
-let timeout = ""
+let timeout = "";
 
 function displayOutput() {
    ciphertextoutput.style.display = "block"
-   spinner.style.display = "none"
+   spinner.style.display = "none";
 }
 
-function loadEffect(){
+function loadEffect() {
    timeout = setInterval(displayOutput, 3000);
    spinner.style.display = "block"
    ciphertextoutput.style.display = "none"
@@ -326,7 +319,7 @@ function loadEffect(){
 
 //    console.log(`this is the key valid function ${valid}`)
 //    return valid
-  
+
 // }
 
 
@@ -334,38 +327,32 @@ function loadEffect(){
 generateBtn.addEventListener('click', () => {
    console.log("this is the decrypt event listener");
    console.log(`the cipher mode is ${cipherMode}`)
- 
+
    let includesSpaces = message.value.includes(" ")
- 
-   if(getMessage() === "" ||getKey() === undefined || includesSpaces )
-      {
-         alert(`Fields cannot be left blank\nFields cannot have spaces\nThe key value must be greater than 0 and less than ${max_key_length}`);
-      }
-  
 
-      else{
+   if (getMessage() === "" || getKey() === undefined || includesSpaces) {
+      alert(`Fields cannot be left blank\nFields cannot have spaces\nThe key value must be greater than 0 and less than ${max_key_length}`);
+   } else {
 
-         if(cipherMode === "encrypt")
-   {
+      if (cipherMode === "encrypt") {
          encryptMessage(getMessage(), getKey());
-      
-   }
-   else {
-      decryptMessage(getMessage(), getKey());
-   }
-   loadEffect();
 
+      } else {
+         decryptMessage(getMessage(), getKey());
       }
+      loadEffect();
+
+   }
 
 
-   
+
 
    //DISPLAY LABEL WHEN BUTTON IS CLICKED
    // outputLabel.style.display = "block"
 
-  
-   
-   
+
+
+
 })
 
 
@@ -377,7 +364,3 @@ generateBtn.addEventListener('click', () => {
 // function displayLoader(){
 //    timeout = setTimeout(displayCipherText, 3000);
 // }
-
-
-
-
